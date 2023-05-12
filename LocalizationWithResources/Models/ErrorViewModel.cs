3a0ba518fb193734aa5848ac6973b7e9f43f1850
milad dotnet <miladhsb932@gmail.com>
2023-05-12 12:28:13 +0330

    Add project files.

---
+++ LocalizationWithResources/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocalizationWithResources.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LocalizationWithResources/Models/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocalizationWithResources.Models
+{
+    public class PersonModel
+    {
+    
+        public int Id { get; set; }
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+    }
+}
